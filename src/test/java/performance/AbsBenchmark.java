8d8cbd989e4683c65d4ef09b9c3d5433539ccc8f
mikera <mike.r.anderson.13@gmail.com>
2012-12-06 19:25:44 +0800

    Updated benchmark

--- src/test/java/performance/AbsBenchmark.java
+++ src/test/java/performance/AbsBenchmark.java
@@ -11,12 +11,28 @@ public class AbsBenchmark extends SimpleBenchmark {
 	public void timeMikeraMathsFloatAbs(int reps) {
 		float c=0;
 		for (int i=0; i<reps; i++) {
-			c+=Maths.abs(i);
+			c+=Maths.abs((float)i);
 		}
 		r=c;
 	}
 	
 	public void timeJavaMathsFloatAbs(int reps) {
+		float c=0;
+		for (int i=0; i<reps; i++) {
+			c+=java.lang.Math.abs((float)i);
+		}
+		r=c;
+	}
+	
+	public void timeMikeraMathsIntAbs(int reps) {
+		float c=0;
+		for (int i=0; i<reps; i++) {
+			c+=Maths.abs(i);
+		}
+		r=c;
+	}
+	
+	public void timeJavaMathsIntAbs(int reps) {
 		float c=0;
 		for (int i=0; i<reps; i++) {
 			c+=java.lang.Math.abs(i);
